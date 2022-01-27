4f241e2a18523ca90651733403fc5bba554f4f2b
jonatha <hef6666@gmail.com>
2022-01-27 16:53:21 -0600

    [Modify] - AboutUs timeline: Update my branch

---
+++ aboutUs/js/aboutUs-video.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#button-drop").click(function () {
+        $("#button-drop").toggleClass("drop-rotate")
+        $("#drop-description").toggleClass("drop-active")
+    })
+})
