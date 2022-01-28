ddc9707ec3b168eb8f8cb7fb675a22932f4bd083
jonatha <hef6666@gmail.com>
2022-01-28 13:24:40 -0600

    new script for  contact from

--- aboutUs/js/aboutUs.js
+++ aboutUs/js/aboutUs.js
@@ -1,11 +1,12 @@
 // aboutUs-Video Dropdown Effect
+/*
 $(document).ready(function () {
     $("#button-drop").click(function () {
         $("#button-drop").toggleClass("drop-rotate");
         $("#drop-description").toggleClass("drop-active");
     });
 });
-
+*/
 // aboutUs-Timeline Gradient Effect
 $(function () {
     window.sr = ScrollReveal({ reset: true });
@@ -22,34 +23,21 @@ $(function () {
 });
 
 // aboutUs-Contact Form Request
-//Requerimos el paquete
-var nodemailer = require("nodemailer");
-
-//Creamos el objeto de transporte
-var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: "hef6666@gmail.com",
-        pass: "avsmkjkfyhnsjpea",
-    },
-});
-
-var mensaje = `
-<h1>¡Hola! Gracias por contactar Estudihambres. </h1>
-<p>Nos contactaremos contigo pronto 🤠</p>
-`;
-
-var mailOptions = {
-    from: "hef6666@gmail.com",
-    to: "hef6666@gmail.com,jonathan.jimenez.27.96@gmail.com",
-    subject: "Asunto Del Correo",
-    html: mensaje,
-};
-
-transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Email enviado: " + info.response);
-    }
-});
+const hola=()=>{
+   let e=document.getElementsByTagName('input')
+   let a=document.getElementById('exampleFormControlTextarea1').value
+  for (let index = 1; index <e.length; index++) {
+     console.log(e[index].value)
+  }
+  Email.send({
+	Host: "smtp.gmail.com",
+	Username : 'hef6666@gmail.com',
+	Password : 'avsmkjkfyhnsjpea',
+	To : `hef6666@gmail.com`,
+	From : `${e[2]}`,
+	Subject : "email subject",
+	Body : `hi my naame is ${e[1]} ${a}`,
+	}).then(
+		message => alert("email sent")
+	);
+}
