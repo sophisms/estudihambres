dbba6bc5b25dcf67d9079b1c36b32690cb247232
jonatha <hef6666@gmail.com>
2022-02-10 17:40:35 -0600

    new functioncreat item

--- mentorsMenu/mentors.js
+++ mentorsMenu/mentors.js
@@ -4,95 +4,101 @@ let mentor={
             name:"luis"
             ,img:"https://images.pexels.com/photos/953703/pexels-photo-953703.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
             ,star:5
-            ,class:"musica"
+            ,class:"Música"
             ,modality:"en linea"
-            ,description: "lorem lorem"
-            ,price:15
-            
+            ,price:150
+           
         },
         {
             name:"juan"
             ,img:"https://images.pexels.com/photos/4307849/pexels-photo-4307849.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             ,star:5
-            ,class:"Pintura"
+            ,class:"Pintura Óleo"
             ,modality:"en linea"
-            ,price:20
-            
+            ,price:200
+           
         },
         {
             name:"mon"
             ,img:"https://images.pexels.com/photos/5795034/pexels-photo-5795034.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             ,star:4
-            ,class:"danza contemporanea"
+            ,class:"Danza Contemporanea"
             ,modality:"en linea"
-            ,price:35
-            
+            ,price:350
+        
         },
         {
             name:"karen"
             ,img:"https://images.pexels.com/photos/6094038/pexels-photo-6094038.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             ,star:5
-            ,class:"ingles"
+            ,class:"Inglés"
             ,modality:"en linea"
-            ,price:35
-            
+            ,price:350
+          
         },
+        
         {
             name:"brenda"
             ,img:"https://images.pexels.com/photos/7307380/pexels-photo-7307380.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             ,star:2
-            ,class:"programaciòn"
+            ,class:"Programación"
             ,modality:"en linea"
-            ,price:25
-            
+            ,price:250
+           
         },
         {
             name:"pablo"
             ,img:"https://images.pexels.com/photos/2033287/pexels-photo-2033287.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             ,star:3
-            ,class:"fisica"
+            ,class:"Electrónica"
             ,modality:"en linea"
-            ,price:30
+            ,price:300
             
         },
         {
             name:"pedro"
             ,img:"https://images.pexels.com/photos/5131401/pexels-photo-5131401.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             ,star:4
-            ,class:"algebra"
+            ,class:"Acuarela"
             ,modality:"en linea"
-            ,price:35
-            
+            ,price:350
+           
         },
         {
             name:"mario"
             ,img:"https://images.pexels.com/photos/4607198/pexels-photo-4607198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             ,star:2
-            ,class:"mecanica"
+            ,class:"Matemáticas"
             ,modality:"en linea"
-            ,price:5
+            ,price:50
             
         },
         {
             name:"luis"
             ,img:"https://images.pexels.com/photos/10887630/pexels-photo-10887630.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             ,star:1
-            ,class:"Historia"
+            ,class:"Electrónica"
             ,modality:"en linea"
-            ,price:15
-            
+            ,price:150
+           
         },
         {
             name:"mayrelin"
             ,img:"https://images.pexels.com/photos/5794945/pexels-photo-5794945.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             ,star:5
-            ,class:"electronica"
+            ,class:"Electrónica"
             ,modality:"en linea"
-            ,price:30
+            ,price:300
+    
         }
     ]
 }
-function inputMentor(item,element,number=item.length) {
+let truc=`
+<div class="col-1 position-absolute top-0 end-0" height="24px">
+<img src="../cog-solid.svg" height="24px" alt="start" onclick=element()>
+</div>
+`;
+function inputMentor(item,element,plus=truc,number=item.length) {
     conten="";
     star=""
   
@@ -113,8 +119,9 @@ function inputMentor(item,element,number=item.length) {
         }
         conten += `
         <div name="item" class=" m-3 position-relative" style="width: 18rem;">
-        <a href="#mentorsCards" data-bs-toggle="modal" onclick=ConstructorModal(${index}) href="#">
+        <a href="#mentorsCards" data-bs-toggle="modal" onclick= ConstructorModal(${index}) href="#">
      <img src="${item[index].img}" class="  card-img-top"  data-bs-toggle="mentorsCards"  alt="..." style="border-radius: 30px;">
+     </a>
     <div class="card-body position-absolute bottom-0  bg-dark bg-opacity-50 text-white col-12" style="width: 16.5rem; border-bottom-left-radius:30px; border-bottom-right-radius:30px;">
           <h5 class="card-title">${item[index].name}</h5>
         <div class="row justify-content-between">
@@ -122,27 +129,28 @@ function inputMentor(item,element,number=item.length) {
             ${item[index].class}
             </div>
         <div class="col-7 text-end">
-              ${item[index].price} dollar/hour
+              ${item[index].price} MNX/Hr
     </div>
 </div>
             <div class="row g-0 justify-content-around"> <!--contenedor estrellas-->
                 ${star}
             </div>
         </div>
-        </a>
       </div>
         `;
         star=""
     }
-    document.getElementById(`${element}`).innerHTML=`
-   <div class="col-12 row g-0 justify-content-around" >
-   <div class="col-6">
-   <button type="button" class="btn btn-primary col-2" onclick=element()>borrar</button>
-   <button type="button" class="btn btn-primary col-2" onclick=addItem()>agregar</button>
-   </div>
-   </div>
-   `+conten
+    document.getElementById(`${element}`).innerHTML=plus+conten
 }
+let bot=`
+<div class="col-12 row g-0 justify-content-around" >
+<div class="col-6">
+<button type="button" class="btn btn-primary col-2" onclick=element()>borrar</button>
+<button type="button" class="btn btn-primary col-2" onclick=addItem()>agregar</button>
+</div>
+</div>
+`;
+
 
 inputMentor(mentor.items,"Cards-Mentores")
 
@@ -155,11 +163,7 @@ let ne= {
     ,price:30
 }
 
-function addItem(array=mentor.items,newItem=ne) {
-    array.push(newItem)
-   inputMentor(mentor.items,"Cards-Mentores")
-}
-git 
+
 function putStart(id,num){
     mentor.items[id].star=num
     inputMentor(mentor.items,"Cards-Mentores")
@@ -169,15 +173,22 @@ function deleteItem(id,object=mentor.items) {
     object.splice(id, 1);
     inputMentor(mentor.items,"Cards-Mentores")
 }
-
+function abort() {
+    inputMentor(mentor.items,"Cards-Mentores")
+}
 function element() {
+    let cont=truc+`
+    <div class="col-12 row g-0 justify-content-around pb-2" height="24px">
+    <button type="button" class="btn btn-primary col-1" data-bs-toggle="modal" data-bs-target="#ModalADD" data-bs-whatever="@mdo">Agregar</button>
+    <button type="button" class="btn btn-danger col-1" onclick=abort()>Cancelar</button>
+    </div>`
+    inputMentor(mentor.items,"Cards-Mentores",cont)
     let item=document.getElementsByName("item")
     item.forEach((items,index)=>{
         let cont=items.innerHTML
         items.innerHTML=`
-        <div class="position-absolute top-0 end-0" onclick=deleteItem(${index})>
-        <p>x</p>
-        </div> 
+        <img class="position-absolute top-0 end-0" src="../trash-can-solid.svg" height="24px" alt="start" onclick=deleteItem(${index})>
+        <img class="position-absolute top-0 start-0" src="../pen-solid.svg" height="24px" alt="start" onclick=ModalEdit(${index}) data-bs-toggle="modal" data-bs-target="#mentorsCards" data-bs-whatever="@mdo">
         `+cont
     })
    
@@ -192,10 +203,125 @@ function ConstructorModal( id, array = mentor.items ){
   <h4>modalidad: ${array[id].modality}</h4>
   <p>tarifas accesibles a tan solo: ${array[id].price} dolares /hora</p>
   <button class="btn  me-3 rounded-pill btn-primary">solicitar clase</button> 
-  `
-  
-
+  `;
 }
 
+function ModalEdit( id, array = mentor.items ){
+    let image = document.getElementById("imageProfile")
+    let contentMentor = document.getElementById("contentProfile")
+    image.style.cssText=`width: 300px; margin:auto;`
+    image.src =`${array[id].img}`
+    contentMentor.innerHTML = `
+    <div class="form-group col">
+        <label for="inputClass">Nombre</label>
+        <div class="row ">
+            <div class="col-5">
+                <input id="name" type="text" class="form-control" value="${array[id].name}" placeholder="Nombre">
+            </div>
+            <div class="col-5">
+                <input id="lastName" type="text" class="form-control" placeholder="Apellido">
+            </div>
+        </div>
+    </div>
 
+    <div class="form-group col">
+        <label for="inputStates">Asignatura</label>
+        <select id="inputState" class="form-control">
+            ${getOptionClass(array[id].class)}
+        </select>
+    </div> 
 
+    <fieldset class="form-group row">
+        <legend class="col-form-label col-3 float-sm-left pt-0">Modalidad</legend>
+        <div class="col-9">
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" checked value="option1">
+                <label class="form-check-label" for="inlineCheckbox1">En line</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" disabled>
+                <label class="form-check-label" for="inlineCheckbox2">Presencial</label>
+            </div>
+        </div> 
+    </fieldset>
+
+    <div class="form-group col">
+        <label for="inputClass">Precio</label>
+        <input id="Price" type="text" class=" col-7 form-control" value="${array[id].price}" aria-label="Dollar amount (with dot and two decimal places)">
+    </div>
+    <div class="modal-footer">
+    <button type="button" class="btn btn-primary" onclick=catchValueEdit(${id}) data-bs-dismiss="modal">Actualizar</button>
+    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+   </div>
+
+        `;
+  }
+  function catchValueEdit(id,items=mentor.items){
+    let imgs="https://thumbs.dreamstime.com/b/omita-el-icono-del-perfil-avatar-placeholder-gris-de-la-foto-99724602.jpg"
+    imgs=document.getElementById("imageProfile")
+    let name=document.getElementById("name")
+    let last=document.getElementById("lastName")
+    let clas=document.getElementById("inputState")
+    let price=document.getElementById("Price")
+    
+    let cont={
+        name:`${name.value}`
+        ,lastName:`${last.value}`
+        ,img:imgs.src
+        ,star:1
+        ,class:`${clas.value}`
+        ,modality:"en linea"
+        ,price:price.value
+    }
+    items[id]=cont
+    inputMentor(mentor.items,"Cards-Mentores")
+}
+function catchValueNew(items=mentor.items){
+    let imgs="https://thumbs.dreamstime.com/b/omita-el-icono-del-perfil-avatar-placeholder-gris-de-la-foto-99724602.jpg"
+    // imgs=document.getElementById("imageProfile")
+    let name=document.getElementById("name")
+    let last=document.getElementById("lastName")
+    let clas=document.getElementById("inputClas")
+    let price=document.getElementById("Price")
+    console.log(clas.value)
+    let cont={
+        name:`${name.value}`
+        ,lastName:`${last.value}`
+        ,img:imgs
+        ,star:1
+        ,class:clas.value
+        ,modality:"en linea"
+        ,price:price.value
+    }
+    items.push(cont)
+    console.log(items)
+   inputMentor(items,"Cards-Mentores")
+   imgs="",name="",last="",clas="",price=""
+  
+}
+
+let optionClass=[
+    {id:1,name:"Música"},
+    {id:2,name:"Matemáticas"},
+    {id:3,name:"Quimica"},
+    {id:4,name:"Canto"},
+    {id:5,name:"Pintura Óleo"},
+    {id:6,name:"Acuarela"},
+    {id:7,name:"Inglés"},
+    {id:8,name:"Danza Contemporanea"},
+    {id:9,name:"Programación"},
+    {id:10,name:"Electrónica"}
+]
+
+function getOptionClass(name,opClass=optionClass){
+    let cont ="";
+    for (let el = 0; el < opClass.length; el++) {
+        if(name==opClass[el].name){
+            cont+=`<option selected value="${name}">${name}</option>`
+        }
+        else{
+            cont+=`<option value="${opClass[el].name}">${opClass[el].name}</option>`
+        }
+    };
+    return (cont)
+}
